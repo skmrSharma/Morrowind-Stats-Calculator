f0e127c1b54f9bb626f54b9ae20e7b7be7c9afd6
skmrSharma <54986978+skmrSharma@users.noreply.github.com>
2020-07-03 11:09:28 +0530

    alchemy success chance has been added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $("ul#starting-values").hide();
-$("form").submit(function(){
+$("form#basic-attributes").submit(function(){
 	var end = +($("input[name='end']").val());
 	var str = +($("input[name='str']").val());
 	var int = +($("input[name='int']").val());
@@ -20,14 +20,34 @@ $("form").submit(function(){
 		magickaMult=0;
 	}
 	var magicka = int * (1 + magickaMult);
-	$("ul#starting-values li").eq(0).html("Health: ");
-	$("ul#starting-values li").eq(0).append(health);
-	$("ul#starting-values li").eq(1).html("Magicka: ");
-	$("ul#starting-values li").eq(1).append(magicka);
-	$("ul#starting-values li").eq(2).html("Fatigue: ");
-	$("ul#starting-values li").eq(2).append(fatigue);
-	$("ul#starting-values li").eq(3).html("Max Encumbrance: ");
-	$("ul#starting-values li").eq(3).append(maxEncumbrance);
+	$("ul#starting-values li").eq(0).html("Health: "+health);
+	//$("ul#starting-values li").eq(0).append(health);
+	$("ul#starting-values li").eq(1).html("Magicka: "+magicka);
+	//$("ul#starting-values li").eq(1).append(magicka);
+	$("ul#starting-values li").eq(2).html("Fatigue: "+fatigue);
+	//$("ul#starting-values li").eq(2).append(fatigue);
+	$("ul#starting-values li").eq(3).html("Max Encumbrance: "+maxEncumbrance);
+	//$("ul#starting-values li").eq(3).append(maxEncumbrance);
 	$("ul#starting-values").show();
 	return false;	// to prevent default behaviour of submit; since action is not specified in <form>, by default submit reloads the page
+});
+
+$(".alchemy").hide();
+
+$("#alchemy-button").click(function(){
+	$(".alchemy").toggle();
+});
+
+$(".alchemy form").submit(function(){
+	var alcSkill = +($("input[name='alchemy-skill']").val());
+	var int = +($("input[name='int']").val());
+	var luck = +($("input[name='luck']").val());
+	var fatigueRatio = +($("input[name='current-fatigue']").val())/+($("input[name='max-fatigue']").val());
+	if(fatigueRatio > 1){
+		alert("Error: Current fatigue should be less than maximum fatigue");
+		return false;
+	}
+	var alcSuccessRate = (alcSkill + int/5 + luck/10) * (0.75 + 0.5*fatigueRatio);
+	$(".alchemy p").html("Alchemy Success Rate = "+alcSuccessRate);
+	return false;
 });
